f3310c57f67ceade4258c16f0506e5eab4ebc4c3
LiveLong24 <bkshet11@gmail.com>
2023-02-08 22:08:44 +0530

    youtube downloader on specific port

--- server.mjs
+++ server.mjs
@@ -1,1 +1,21 @@
-console.log('in server.mjs git snode module js');
+import express from 'express';
+import cors from 'cors';
+import ytdl from 'ytdl-core';
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.listen(port, ()=>{
+    console.log(`Server runs at ${port}`);
+});
+
+app.get('/download', (req,res) => {
+    var responseUrl = req.query.URL;
+    //res.json({url:responseUrl});
+    res.header('Content-Disposition', `attachment; filename="video.mp4"`);
+
+    ytdl(responseUrl, {
+        format: 'mp4'
+    }).pipe(res);
+});
