8e11dbcdea07e7c5a9d7d4b70b04cfa6ee25c5ff
LiveLong24 <bkshet11@gmail.com>
2023-02-08 22:12:45 +0530

    dynamic port added

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import cors from 'cors';
 import ytdl from 'ytdl-core';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.listen(port, ()=>{
